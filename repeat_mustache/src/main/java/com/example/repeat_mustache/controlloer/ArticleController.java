b27d50b2737937fcba6826c4a508c5dbc8f5d84a
cvivis <hsy0754@naver.com>
2022-11-10 12:02:19 +0900

    [기능] findById 구현

--- repeat_mustache/src/main/java/com/example/repeat_mustache/controlloer/ArticleController.java
+++ repeat_mustache/src/main/java/com/example/repeat_mustache/controlloer/ArticleController.java
@@ -5,9 +5,13 @@ import com.example.repeat_mustache.domain.entity.Article;
 import com.example.repeat_mustache.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @Slf4j
@@ -15,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ArticleController {
 
     private final ArticleRepository articleRepository;
-
+// ArticleRepository는 interface지만 그 구현체(ArticleDao)를 SpringBoot가 넣어줍니다.
     public ArticleController(ArticleRepository articleRepository) {
         this.articleRepository = articleRepository;
     }
@@ -25,6 +29,20 @@ public class ArticleController {
         return "articles/new";
 
     }
+
+    @GetMapping(value = "/{id}")
+    public String getIdPage(@PathVariable Long id, Model model){
+        Optional<Article> optArticle = articleRepository.findById(id); // null 값이 들어와도 nullPointExecption 발생 X
+        if (!optArticle.isEmpty()) { // null 이 아닐때
+            // Optional.get() ---> Article
+            model.addAttribute("article", optArticle.get()); // view article에 값 전달.
+            return "articles/show";
+        } else {
+            return "articles/error";
+        }
+    }
+
+
 
     @PostMapping(value = "/posts")
     public String createArticle(ArticleDto form){
